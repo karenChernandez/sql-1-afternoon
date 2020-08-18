60c9da35e4d3705f727e07eb333f5892fe504824
karenChernandez <karenChernandez@live.com>
2020-08-18 13:35:07 -0700

    completed SQL-1

--- init.sql
+++ init.sql
@@ -1,19 +1,53 @@
 -- Table - person
 -- #1
+CREATE TABLE person
+(person_id SERIAL PRIMARY KEY, 
+name VARCHAR(200), 
+age INT, 
+height INT, 
+city VARCHAR(200), 
+favorite_color VARCHAR(200));
 -- #2
+INSERT INTO person
+(name, age, height, city, favorite_color)
+VALUES
+('Karen Hernandez', 30, 175, 'Yuma', 'Black')
 -- #3
+SELECT * FROM person
+ORDER BY height DESC;
 -- #4
+SELECT * FROM person
+ORDER BY height ASC;
 -- #5
+SELECT * FROM person
+ORDER BY age ASC;
 -- #6
+SELECT * FROM person
+WHERE age > 20;
 -- #7
+SELECT * FROM person
+WHERE age = 18;
 -- #8
+SELECT * FROM person
+WHERE age <20 OR age >30;
 -- #9
+SELECT * FROM person
+WHERE  age != 27;
 -- #10
+SELECT * FROM person
+WHERE favorite_color != 'red';
 -- #11
+SELECT * FROM person
+WHERE favorite_color != 'red' OR favorite_color != 'blue';
 -- #12
+SELECT * FROM person
+WHERE favorite_color = 'orange' OR favorite_color = 'green';
 -- #13
+SELECT * FROM person
+WHERE favorite_color IN('orange', 'green', 'blue')
 -- #14
-
+SELECT * FROM person
+WHERE favorite_color IN('yellow', 'purple')
 
 -- Table -Orders
 -- #1
